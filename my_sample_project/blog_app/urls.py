1d9fe6a237d21046de658c6407414f61450aa4e5
Victor Sklyar <agamaontherock@gmail.com>
2024-11-11 09:25:35 +0200

    feat: pagination and slug

--- my_sample_project/blog_app/urls.py
+++ my_sample_project/blog_app/urls.py
@@ -5,8 +5,9 @@ app_name="blog_app"
 
 urlpatterns = [
     path('posts/', BlogListView.as_view(), name="posts"),
-    path('post/<int:pk>', BlogDetailView.as_view(),name="post"),
+    path('post/<int:year>/<int:month>/<int:day>/<slug:slug_id>', BlogDetailView.as_view(),name="post"),
+    path('post/<int:pk>', BlogDetailView.as_view(),name="post_by_pk"),
     path('delete_post/<int:pk>', BlogDeleteView.as_view(),name="delete_post"),
     path('update_post/<int:pk>',BlogUpdateView.as_view() ,name="update_post"),
-    # path('create_post', ,name="create_post"),
+    path('create_post/', BlogPostCreateView.as_view() ,name="create_post"),
 ]
--- my_sample_project/blog_app/views.py
+++ my_sample_project/blog_app/views.py
@@ -1,31 +1,45 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic import DetailView, DeleteView, UpdateView
+from django.views.generic import DetailView
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
 from .models import BlogPost
+from django.http import Http404
+from .ownerviews import *
 
 class BlogListView(ListView):
     model = BlogPost
+    paginate_by = 2
 
 class BlogDetailView(DetailView):
     model = BlogPost
     
-class BlogDeleteView(DeleteView):
+    def get_object(self, queryset=None):
+        pk = self.kwargs.get("pk")
+        
+        year = self.kwargs.get("year")
+        month = self.kwargs.get("month")
+        day = self.kwargs.get("day")
+        slug_id = self.kwargs.get("slug_id")
+
+        if pk:
+            return get_object_or_404(self.model, pk=pk)
+        elif slug_id:
+            return get_object_or_404(self.model, created_at__year=year, created_at__month=month, created_at__day=day, slug = slug_id)
+        else:
+            raise Http404("No object found matching the provided criteria.")
+
+class BlogDeleteView(OwnerDeleteView):
     model = BlogPost
     success_url = reverse_lazy("blog_app:posts")
+   
+class BlogPostCreateView(OwnerCreateView):
+    model = BlogPost
+    fields = ["title", "text"]
+    success_url = reverse_lazy("blog_app:posts")
     
-    def get_queryset(self, **kwargs):
-        qset = super().get_queryset(**kwargs)
-        qset = qset.filter(owner = self.request.user)
-        return qset
-    
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(OwnerUpdateView):
     model = BlogPost
     success_url = reverse_lazy("blog_app:posts")
     fields = ["title", "text"]
     # fields = "__all__"
-    
-    def get_queryset(self, **kwargs):
-        qset = super().get_queryset(**kwargs)
-        qset = qset.filter(owner = self.request.user)
-        return qset
