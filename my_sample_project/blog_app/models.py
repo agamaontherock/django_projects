ea519394bf28b9945864bde9ae5e1d345da5d2d4
Victor Sklyar <agamaontherock@gmail.com>
2024-11-12 08:19:05 +0200

    feat: add pagination and slug

--- my_sample_project/blog_app/models.py
+++ my_sample_project/blog_app/models.py
@@ -1,14 +1,27 @@
 from django.db import models
 from django.conf import settings
 from django.core.validators import *
-
+from django.urls import reverse
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200, validators=[MinLengthValidator(10)])
     text = models.TextField(validators=[MinLengthValidator(10)])
+    slug = models.SlugField(unique_for_date = 'created_at')
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def get_absolute_url(self):
+        return reverse(
+            'blog_app:post_by_slug',
+            args=[
+                self.created_at.year,
+                self.created_at.month,
+                self.created_at.day,
+                self.slug
+                ]
+            )
+        # return reverse("blog_app:post", kwargs={"pk": self.pk})
+    
     def __str__(self):
         return self.title
--- my_sample_project/blog_app/views.py
+++ my_sample_project/blog_app/views.py
@@ -4,6 +4,9 @@ from django.views.generic import DetailView, UpdateView, DeleteView, CreateView
 from .models import BlogPost
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+from django.utils.text import slugify
 
 class BlogPostCreateView(LoginRequiredMixin, CreateView):
     model = BlogPost
@@ -14,11 +17,13 @@ class BlogPostCreateView(LoginRequiredMixin, CreateView):
         print('form_valid called')
         object = form.save(commit=False)
         object.owner = self.request.user
+        object.slug = slugify(object.title)
         object.save()
         return super(BlogPostCreateView, self).form_valid(form)
     
 class BlogListView(ListView):
     model = BlogPost
+    paginate_by = 2
     
     # def get_queryset(self, **kwargs):
     #    crazy = BlogPost.objects.filter(title__startswith = "Q") # Convention: Car
@@ -32,6 +37,25 @@ class BlogListView(ListView):
     
 class BlogDetailView(DetailView):
     model = BlogPost
+    
+    def get_object(self, queryset=None):
+        pk = self.kwargs.get("pk")
+        
+        year = self.kwargs.get("year")
+        month = self.kwargs.get("month")
+        day = self.kwargs.get("day")
+        slug_id = self.kwargs.get("slug_id")
+
+        if pk:
+            return get_object_or_404(self.model, pk=pk)
+        elif slug_id:
+            return get_object_or_404(self.model, 
+                                     created_at__year=year, 
+                                     created_at__month=month, 
+                                     created_at__day=day, 
+                                     slug = slug_id)
+        else:
+            raise Http404("No object found matching the provided criteria.")
 
 class BlogUpdateView(UpdateView):
     model = BlogPost
