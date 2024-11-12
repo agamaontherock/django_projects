8c5aae3f7de6533778a81c597bd61a7544a25954
Victor Sklyar <agamaontherock@gmail.com>
2024-11-12 08:23:25 +0200

    fix: wrong value for unique_for_date

--- my_sample_project/blog_app/models.py
+++ my_sample_project/blog_app/models.py
@@ -7,7 +7,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    slug = models.SlugField(unique_for_date = True, blank=False)
+    slug = models.SlugField(unique_for_date = 'created_at', blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
