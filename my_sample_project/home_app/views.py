16e9e286fed6bb079ccbe316e6cf0b5d63809e6e
Victor Sklyar <agamaontherock@gmail.com>
2024-12-02 13:06:31 +0200

    feat: accout edit form was added

--- my_sample_project/home_app/views.py
+++ my_sample_project/home_app/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+
+from .forms import *
 
 # Create your views here.
 def home(request):
@@ -14,6 +17,7 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            Profile.objects.create(user=user)
             login(request, user)  # Log in the user immediately after registration
             return redirect('home_app:home')  # Redirect to a 'home' page after registration
     else:
@@ -27,9 +31,26 @@ def open_page(request):
 def closed_page(request):
     return HttpResponse("<h1>Closed page</h1><p>This page is available only to authorized users. <br> You are definitely authorized if you see this page.</p>")
 
+@login_required
 def edit_account(request):
+    # user = User.objects.get(pk=request.user.id)
+    if request.method == "POST":
+        user_edit_form = EditUserForm(instance=request.user, 
+                                      data = request.POST)
+        profile_edit_form = EditProfileForm(instance=request.user.profile, 
+                                            data = request.POST,
+                                            files = request.FILES)
+        
+        if user_edit_form.is_valid() and profile_edit_form.is_valid():
+            user_edit_form.save()
+            profile_edit_form.save()
+    else:
+        user_edit_form = EditUserForm(instance=request.user)
+        profile_edit_form = EditProfileForm(instance=request.user.profile)
+        
     return render(request, 
-                  'registration/account_edit.html',
+                  'registration/account_edit_form.html',
                   {
-                      
+                      "user_edit_form" : user_edit_form,
+                      "profile_edit_form" : profile_edit_form
                   })
